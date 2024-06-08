f13a94795ef2324c6ab217e05794a41e8df35716
a0088869 <eduardo.navas@telefonica.com>
2024-06-08 19:06:29 -0300

    add new functionality

--- src/main/java/br/com/tlf/edutech/msedutechb/core/application/service/BillServiceUseCaseImpl.java
+++ src/main/java/br/com/tlf/edutech/msedutechb/core/application/service/BillServiceUseCaseImpl.java
@@ -4,24 +4,23 @@ import br.com.tlf.edutech.msedutechb.api.billservice.rest.dto.BillDTO;
 import br.com.tlf.edutech.msedutechb.api.exception.BillIdExistException;
 import br.com.tlf.edutech.msedutechb.core.port.in.BillServiceUseCase;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class BillServiceUseCaseImpl implements BillServiceUseCase {
 
-    @Autowired
-    private Set<String> billIdRepoCache;
+    private static Set<String> billIdRepoCache;
 
     @Value("${wait}")
     private long wait;
 
     @SneakyThrows
     @Override
-    public void processBill(BillDTO billDTO) {
+    public synchronized void processBill(BillDTO billDTO) {
         Thread.sleep(wait);
         if(!billIdRepoCache.add(billDTO.getId())){
             throw new BillIdExistException(String.format("Bill Id: %s exists", billDTO.getId()));
@@ -31,6 +30,6 @@ public class BillServiceUseCaseImpl implements BillServiceUseCase {
     @Override
     public void cleanIds() {
 
-        billIdRepoCache.removeAll(billIdRepoCache);
+        billIdRepoCache = new HashSet<>();
     }
 }
